86d3863ea8e1528eda9a0db7a37ad9c3c0c35b08
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-08-11 17:39:58 -0400

    Initial commit.

---
+++ start.lua
@@ -0,0 +1,2 @@
+--- Entry point for the game pack management system.
+blight.output("Welcome to the blightmud experience management system!", "To launch a game, please bgype the name of the game.", "To see the list of experiences that have been written, please type /explist")
