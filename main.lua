aee44961c1fde0a56b541092d58e3a1e669042f6
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-08-15 11:31:56 -0400

    Expose the plugin's main Lua script to the BlightMUD world so it will run things.

--- main.lua
+++ main.lua
@@ -1,14 +1,13 @@
 --- Entry point for the game pack management system.
-local mod = {}
-mod.alias_table = alias.add_group() -- Our alias table for registration
-mod.experiences = {}
-mod.Record = require "record"
-function mod.greet()
+alias_table = alias.add_group() -- Our alias table for registration
+experiences = {}
+Record = require "record"
+function greet()
   blight.output("Welcome to the BlightMUD experience management system, brought to you by Hunter Jozwiak and contributors.")
   blight.output("Please enter the name of a game to launch it, or /explist to list the experiences you have available.")
 end
 
-function mod.read_experience_files()
+function read_experience_files()
   local experiences = {}
   local handle = io.open("EXPERIENCES", "r")
   for line in handle:read() do
@@ -18,7 +17,7 @@ function mod.read_experience_files()
       if rawlen(dets) < 4 then
         blight.output("Malformed record, skipping this.")
         else
-local rec = mod.Record.create(table.unpack(dets))
+local rec = Record.create(table.unpack(dets))
 table.insert(experiences, rec)
 end
     end
@@ -31,4 +30,3 @@ function mod.main()
   mod.greet()
   mod.experiences = mod.read_experience_files()
 end
-return mod
