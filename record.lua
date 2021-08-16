be9385e06b81172960f988a88ec15d65c5e013ce
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-08-16 12:39:11 -0400

    Tentative alias creation functionality.

--- record.lua
+++ record.lua
@@ -1,9 +1,10 @@
+local utils = require "utils"
 -- A record structure for management of packs.
 -- A record consists of the following fields:
 -- * a name by which to launch the experience.
 -- * the stem of the url to get the pack
 -- * the username of the person
--- the repository of the package itself.
+-- * the repository of the package itself.
 
 local Record = {}
 Record.__index = Record
@@ -16,6 +17,8 @@ function Record.create(launch_command, repo_base, username, repo_name)
   this.repo_name = repo_name
   return this
 end
+
+
 function Record:is_present()
   local plugins = plugin.get_all()
   for _,plugin in ipairs(plugins) do
@@ -24,6 +27,28 @@ function Record:is_present()
     end
   end
   return false
+end
+
+function Record:create_alias(group)
+  local format = string.format
+  local regex = regex.new(format("^%s$", self.launch_command))
+  local cb = function (_)
+    -- presence check
+    if not self:is_present() then
+      if utils.y_or_n_p("The package is not yet installed. Would you like to install it?" == "y") then
+        blight.output("Installing the plugin...")
+        plugin.add(format("https://%s/%s/%s", self.repo_base, self.username, self.repo_name))
+      else
+        blight.output("Sorry, this package cannot be used.")
+        return nil
+      end
+
+      else
+        plugin.load(format("%s", self.repo_name))
+    end
+  end
+  -- Now to create the actual alias
+  group:add(regex, cb)
 end
 
 function Record:print()
