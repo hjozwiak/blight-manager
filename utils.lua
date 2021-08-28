925b0036e50c00d1deeebb1006d7912ac1ccb562
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-08-28 12:33:09 -0400

    Update utils.

--- utils.lua
+++ utils.lua
@@ -3,17 +3,17 @@ local mod = {}
 local format = string.format
 function mod.handle_launch(matches)
   local cmd = matches[1]
-  local rec = Record.find_by_launch_command(experiences, cmd)
+  local exp = Experience.find_by_launch_command(experiences, cmd)
   if rec ~= nil then 
   -- presence check
-  if not rec:is_present() then
+  if not exp:is_present() then
 
-    plugin.add(format("https://%s/%s/%s", rec.repo_base, rec.username, rec.repo_name))
+    plugin.add(format("https://%s/%s/%s", exp.repo_base, exp.username, exp.repo_name))
   else
-    plugin.load(format("%s", rec.repo_name))
+    plugin.load(format("%s", exp.repo_name))
   end
   else
-    blight.output("Something bad happened and the record was not found.")
+    blight.output("Something bad happened and the experience was not found.")
     end
 end
 
